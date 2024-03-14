e6b500fb2faecb8581575b25d8ba25fe28dee170
DevmanYur <142353662+DevmanYur@users.noreply.github.com>
2024-03-14 18:06:55 +0300

    up

---
+++ main.py
@@ -0,0 +1,29 @@
+import os
+
+import requests
+from pathlib import Path
+from requests import HTTPError
+from pathvalidate import sanitize_filename
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+from urllib.parse import urlparse
+from urllib.parse import unquote
+
+def download_genre(url):
+    url = 'https://tululu.org/b5'
+
+
+    response = requests.get(url)
+    response.raise_for_status()
+
+    soup = BeautifulSoup(response.text, 'lxml')
+
+    comm = soup.find('span', class_='d_book').text
+    print(comm)
+
+
+if __name__ == '__main__':
+    url55 = 'https://tululu.org/b9'
+
+    download_genre(url55)
+
